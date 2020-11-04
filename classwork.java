aebdf8f3602a9a37e6bbc69023a5ce730796c120
Kaylah Tan <54913238+kaylahtan@users.noreply.github.com>
2020-11-04 13:47:05 -0800

    Create classwork.java

---
+++ classwork.java
@@ -0,0 +1,14 @@
+public class middleAverage{
+  
+}
+public Double middleAverage (int [] a){
+  
+}
+int length= a.length; 
+int b = length/4
+
+for(int i=b; i<a,length - b; i++{
+  sum = sum + i;{
+    
+  }
+})
